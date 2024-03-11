6a4ce318a3a18d2fa647f2b59d508308dca890c3
saumil8200 <saumil8200@gmail.com>
2024-03-11 14:11:58 +0530

    implemented local storage support

--- src/app/store.js
+++ src/app/store.js
@@ -14,7 +14,11 @@ const persistedReducer = persistReducer(persistConfig, todoReducer);
 export const store = configureStore({
     reducer: persistedReducer,
     devTools: process.env.NODE_ENV !== 'production',
-    middleware: [thunk],
+    // middleware: [thunk],
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   })
   
   export const persistor = persistStore(store)
