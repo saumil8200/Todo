f76896f6d83031e1d915796654677f9ae43d07eb
saumil8200 <saumil8200@gmail.com>
2024-03-11 11:35:05 +0530

    redux persist localstorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,21 @@
 import {configureStore} from '@reduxjs/toolkit'
 import todoReducer from '../features/todo/todoSlice'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
 
 export const store = configureStore({
-    reducer: todoReducer
-})
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [applyMiddleware(thunk)]
+  })
+  
+  export const persistor = persistStore(store)
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './app/store.js'
+import { persistor, store } from './app/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 )
